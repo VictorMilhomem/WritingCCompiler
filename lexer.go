cf0d87d5071a551a35249c02bef7f59d391c451a
Victor Milhomem <victormllosada@gmail.com>
2024-11-05 16:02:21 -0300

    lexer completed

--- lexer.go
+++ lexer.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 type TokenKind int
 
 const (
 	EOF TokenKind = iota
+	ERROR
 	ID
-	CONSTANT
+	NUMBERINT
+	NUMBERFLOAT
 	RPAREN
 	LPAREN
 	RBRACE
@@ -26,6 +31,22 @@ var keywords = map[string]TokenKind{
 	"return": RETURN,
 }
 
+var kindString = map[TokenKind]string{
+	EOF:         "EOF",
+	ERROR:       "ERROR",
+	ID:          "ID",
+	NUMBERINT:   "INTEGER",
+	NUMBERFLOAT: "FLOAT",
+	RPAREN:      "RPAREN",
+	LPAREN:      "LPAREN",
+	RBRACE:      "RBRACE",
+	LBRACE:      "LBRACE",
+	SEMICOLON:   "SEMICOLON",
+	INT:         "KEYWORD",
+	VOID:        "KEYWORD",
+	RETURN:      "KEYWORD",
+}
+
 type Location struct {
 	Start, End int
 	Text, File string
@@ -34,7 +55,6 @@ type Location struct {
 type Token struct {
 	Kind     TokenKind
 	Text     string
-	Loc      Location
 	FullText string
 }
 
@@ -50,12 +70,12 @@ func NewLexer(filename, input string) *Lexer {
 	return &Lexer{Filename: filename, Input: input}
 }
 
-func NewToken(kind TokenKind, text string, loc Location, fullText string) Token {
-	return Token{Kind: kind, Text: text, Loc: loc, FullText: fullText}
+func NewToken(kind TokenKind, text string, fullText string) Token {
+	return Token{Kind: kind, Text: text, FullText: fullText}
 }
 
 func (t Token) String() string {
-	return fmt.Sprintf("Token (kind: '%s', text: '%s')", t.Kind, t.Text)
+	return fmt.Sprintf("Token (kind: '%s', text: '%s')", kindString[t.Kind], t.Text)
 }
 
 func (l Location) LocStart() int {
@@ -72,4 +92,108 @@ func (l Location) LocText() string {
 
 func (l Location) LocFile() string {
 	return l.File
+}
+
+func (l *Lexer) Tokenizer() []Token {
+	for {
+		l.Start = l.Pos
+
+		if l.Pos >= len(l.Input) {
+			l.Tokens = append(l.Tokens, Token{Kind: EOF})
+			break
+		}
+
+		if !l.NextToken() {
+			break
+		}
+	}
+
+	return l.Tokens
+}
+
+func (l *Lexer) NextToken() bool {
+	switch c := l.Peek(); c {
+	case ' ', '\t', '\r':
+	case '(':
+		l.Tokens = append(l.Tokens, Token{Kind: LPAREN, Text: string(c)})
+	case ')':
+		l.Tokens = append(l.Tokens, Token{Kind: RPAREN, Text: string(c)})
+	case '{':
+		l.Tokens = append(l.Tokens, Token{Kind: LBRACE, Text: string(c)})
+	case '}':
+		l.Tokens = append(l.Tokens, Token{Kind: RBRACE, Text: string(c)})
+	case ';':
+		l.Tokens = append(l.Tokens, Token{Kind: SEMICOLON, Text: string(c)})
+	default:
+		if unicode.IsLetter(c) {
+			return l.Identifier()
+		} else if unicode.IsDigit(c) {
+			return l.Number()
+		}
+		l.Tokens = append(l.Tokens, l.MakeToken(ERROR))
+	}
+	l.Pos++
+	return true
+}
+
+func (l *Lexer) MakeToken(kind TokenKind) Token {
+	text := l.Input[l.Start:l.Pos]
+
+	token := NewToken(kind, text, text)
+	return token
+}
+
+func (l *Lexer) Identifier() bool {
+	l.Advance(1) // skip the first letter
+
+	for !l.Eof() && IsIdSegment(l.Peek()) {
+		l.Advance(1)
+	}
+
+	id := l.Input[l.Start:l.Pos]
+
+	if keyword, ok := keywords[id]; ok {
+		l.Tokens = append(l.Tokens, l.MakeToken(keyword))
+	} else {
+
+		l.Tokens = append(l.Tokens, l.MakeToken(ID))
+	}
+	return true
+}
+
+func (l *Lexer) Number() bool {
+	l.Advance(1)
+	for !l.Eof() && unicode.IsDigit(l.Peek()) {
+		l.Advance(1)
+
+		if !l.Eof() && l.Peek() == '.' {
+			l.Advance(1)
+			for !l.Eof() && unicode.IsDigit(l.Peek()) {
+				l.Advance(1)
+			}
+			l.Tokens = append(l.Tokens, l.MakeToken(NUMBERFLOAT))
+		}
+	}
+	l.Tokens = append(l.Tokens, l.MakeToken(NUMBERINT))
+	return true
+}
+
+func (l *Lexer) Peek() rune {
+	return rune(l.Input[l.Pos])
+}
+
+func (l *Lexer) Advance(n int) {
+	l.Pos += n
+}
+
+func (l *Lexer) Eof() bool {
+	return l.Pos >= len(l.Input)
+}
+
+func (l *Lexer) LookAhead(n int) rune {
+	return rune(l.Input[l.Pos+n])
+}
+
+func IsIdSegment(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' || r == '.' || r == '\''
 }
--- main.go
+++ main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
-func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+func GetFileSource(fpath string) string {
+	bytes, err := ioutil.ReadFile(path.Base(fpath))
+	source := string(bytes)
+	if err != nil {
+		log.Fatalf("could not open file %s", fpath)
 	}
+	return source
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	filepath := "C:\\Users\\victo\\GolandProjects\\RioLang\\tests\\simple.c"
+	source := GetFileSource(filepath)
+	lexer := NewLexer(filepath, source)
+	tokens := lexer.Tokenizer()
+
+	fmt.Printf("%v", tokens)
+}
